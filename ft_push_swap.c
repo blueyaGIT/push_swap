bdbc7ba1d42d9a8f944c86ecf359f1ff65f4dd9b
Davide Albano <dalbano@1-A-26.42heilbronn.de>
2024-11-06 18:42:47 +0100

    first attempt at quicksort implementation. why: 1. Time efficiency: - Bubblesort X -> O(n^2) = too slow and too many excessive operations - Quicksort YES -> O(log n) or O(nlogn) in best case = quicker and less performance heavy on the system for big stacks (anticipating stack size of 500+) 2. Code complexity - Quicksort -> Implementation not too difficult and should be quick to code - Radix Sort -> Thought that it would be harder to code but now that I think about it, it might be easier. Also quicker O(kn) - Bogo Sort -> gonna do this one for fun ANYWAYS COFFEE BREAK

--- ft_push_swap.c
+++ ft_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:19:50 by dalbano           #+#    #+#             */
-/*   Updated: 2024/11/04 17:12:40 by dalbano          ###   ########.fr       */
+/*   Updated: 2024/11/06 18:34:13 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,57 @@
 #include "libft/libft.h"
 #include "printf/ft_printf.h"
 
-int	ft_push_swap(int i)
+#include <stdio.h>
+
+void	quicksort_stack(t_stack *a, t_stack *b, int start, int end)
 {
-	
+	int	pivot;
+	int	left;
+	int	right;
+
+	pivot = find_median(a, start, end);
+	left = start;
+	right = end;
+	if (end - start <= 2)
+	{
+		// Use direct sorting for small partitions
+		if (a->data[start] > a->data[start + 1])
+			sa(a);
+		if (end - start == 2 && a->data[start + 1] > a->data[start + 2])
+		{
+			ra(a);
+			sa(a);
+			rra(a);
+		}
+		return ;
+	}
+	// Partitioning: use a pivot value to divide into two sections
+
+	// Move elements to `b` based on the pivot
+	while (left <= right)
+	{
+		if (a->data[a->top] < pivot)
+			pb(a, b); // Push to `b` if less than pivot
+		else
+			ra(a); // Rotate in `a` to keep elements >= pivot in place
+	}
+	// Recursively apply to partitions
+	quicksort_stack(a, b, start, left - 1);// Left side
+	quicksort_stack(a, b, right + 1, end);// Right side
+
+	// Merging sorted `b` back to `a`
+	while (b->top >= 0)
+		pa(a, b);// Move sorted elements back to `a`
+	}
+
+int main(int argc, char **argv)
+{
+	t_stack	a;
+	t_stack	b;
+
+	// Initialize stacks with input data here
+
+	// Start the quicksort-like sorting algorithm
+	quicksort_stack(&a, &b, 0, a.top);
+	return (0);
 }
