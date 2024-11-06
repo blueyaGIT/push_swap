b292f885545431bff3a272a0ad71e34cb7925b32
Davide Albano <dalbano@1-A-26.42heilbronn.de>
2024-11-06 18:45:00 +0100

    adjusted push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:03:52 by dalbano           #+#    #+#             */
-/*   Updated: 2024/11/04 17:10:52 by dalbano          ###   ########.fr       */
+/*   Updated: 2024/11/06 18:44:09 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,19 @@
 // # include "libft/libft.h"
 // # include "printf/ft_printf.h"
 
-int	ft_push_swap(int i);
+int		ft_push_swap(int i);
+void	sa(t_stack *a);
+void	pa(t_stack *a, t_stack *b);
+void	pb(t_stack *a, t_stack *b);
+void	ra(t_stack *a);
+void	rra(t_stack *a);
+
+typedef struct s_stack {
+	int	*data;
+	int	top;
+}	t_stack;
+
+int		find_median(t_stack *stack, int start, int end);
+void	quicksort_stack(t_stack *a, t_stack *b, int start, int end);
 
 #endif /* PUSH_SWAP_H */
