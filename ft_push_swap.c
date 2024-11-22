4e9081df222f366c9071f218fd739363ab861cdc
Davide Albano <albanodavide50@gmail.com>
2024-11-22 18:07:33 +0100

    added print_stack() to test my sorting -> currently not working -> coffee

--- ft_push_swap.c
+++ ft_push_swap.c
@@ -6,16 +6,28 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:19:50 by dalbano           #+#    #+#             */
-/*   Updated: 2024/11/22 17:54:02 by dalbano          ###   ########.fr       */
+/*   Updated: 2024/11/22 18:07:03 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	print_stack(t_stack *stack)
+{
+	t_stack	*current;
+
+	current = stack;
+	while (current != NULL)
+	{
+		ft_printf("Number: %d, Index: %d\n", current->nbr, current->index);
+		current = current->next;
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	t_stack	a;
-	t_stack	b;
+	t_stack	*a;
+	t_stack	*b;
 	int		temp;
 
 	temp = 0;
@@ -29,13 +41,14 @@ int	main(int argc, char **argv)
 	while (++temp < argc)
 		ft_push(&a, ft_atoi(argv[temp]), temp - 1);
 	if (temp - 1 > 3)
-		mechanical_turk_sort(&a, &b);
+		mechanical_turk_sort(a, b);
 	else if (temp - 1 == 3)
-		sort_three(&a);
+		sort_three(a);
 	else if (temp - 1 == 2)
-		sort_two(&a);
+		sort_two(a);
 	else
 		ft_printf("Sorting not implemented for this number of elements.\n");
-	free_stacks(&a, &b);
+	print_stack(a);
+	free_stacks(a, b);
 	return (0);
 }
