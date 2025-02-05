22301c74345ac3b1170d97a7548cada29ed1ec49
blueyaGIT <albanodavide50@gmail.com>
2025-02-05 17:41:54 +0100

    norm comp now HAHAHAAHAHHAHAHAH

--- srcs/sort_b.c
+++ srcs/sort_b.c
@@ -6,7 +6,7 @@
 /*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 18:16:27 by dalbano           #+#    #+#             */
-/*   Updated: 2025/02/05 17:25:49 by dalbano          ###   ########.fr       */
+/*   Updated: 2025/02/05 17:41:17 by dalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,11 +48,10 @@ void	sort_b_small(t_stack **a, t_stack **b)
 		while (temp >= 0)
 		{
 			if (!stack)
-			{
 				pb(a, b);
+			if (!stack)
 				break ;
-			}
-			if (temp == calc_ra_rb(*a, *b, stack->nbr))
+			else if (temp == calc_ra_rb(*a, *b, stack->nbr))
 				temp = do_ra_rb(a, b, stack->nbr, 1);
 			else if (temp == calc_rra_rrb(*a, *b, stack->nbr))
 				temp = do_rra_rrb(a, b, stack->nbr, 1);
